aedc5fa03300d972f214e79f70b0a3a9e5087488
bohdan leonets <bohdan@mytowerapp.com>
2023-02-24 15:26:51 +0200

    add settings for eslint and vue-router

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,5 +7,54 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  rules: {
+    // 0 - "off"
+    // 1 - "warn"
+    // 2 - "error"
+    'semi': [2, 'always'],
+    'comma-dangle': [2, 'always-multiline'],
+    'indent': ['error', 2, {
+      'SwitchCase': 1,
+      'MemberExpression': 1,
+      'flatTernaryExpressions': false,
+    }],
+    'vue/no-v-html': 0,
+    'vue/valid-v-on': 0,
+    'vue/multiline-html-element-content-newline': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/html-closing-bracket-spacing': 0,
+    'vue/v-slot-style': 0,
+    'vue/html-self-closing': 2,
+    'vue/new-line-between-multi-line-property': 2,
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 4,
+    }],
+    'vue/require-default-prop': 0,
+    'vue/no-mutating-props': 0, // TODO: looks like it useful, prevent and fix warnings in future
+    'vue/html-indent': ['warn', 2],
+    'vue/no-v-text-v-html-on-component': 0,
+    'vue/valid-v-slot': ['error', { 'allowModifiers': true }],
+    'vue/multi-word-component-names': 1,
+    'vue/attributes-order': [2, {
+      'order': [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'GLOBAL',
+        ['UNIQUE', 'SLOT'],
+        'TWO_WAY_BINDING',
+        'OTHER_DIRECTIVES',
+        'OTHER_ATTR',
+        'EVENTS',
+        'CONTENT',
+      ],
+      'alphabetical': false,
+    }],
+    'vue/match-component-file-name': [1, {
+      'extensions': ['vue', 'js'],
+    }],
+  },
 }
+
