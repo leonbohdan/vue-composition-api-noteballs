6c8183b1959180702d44fd81a3c8e78da7690010
bohdan leonets <bohdan@mytowerapp.com>
2023-02-24 17:30:19 +0200

    add bulma design

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
-import VueNotes from '@/views/VueNotes.vue';
+import ViewNotes from '@/views/ViewNotes.vue';
 import ViewStats from '@/views/ViewStats.vue';
 
 const routes = [
   {
     path: '/',
     name: 'notes',
-    component: VueNotes,
+    component: ViewNotes,
   }, {
     path: '/stats',
     name: 'stats',
