0a3db8e6048730f1563820bac75b90c70d040635
bohdan leonets <bohdan@mytowerapp.com>
2023-03-12 19:21:58 +0200

    add navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-
+import { useStoreAuth } from '@/stores/storeAuth.js';
 import ViewNotes from '@/views/ViewNotes.vue';
 import ViewStats from '@/views/ViewStats.vue';
 import ViewEditNote from '@/views/ViewEditNote.vue';
@@ -28,6 +28,18 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from) => {
+  const storeAuth = useStoreAuth();
+
+  if (!storeAuth.user && to.name !== 'auth') {
+    return { name: 'auth' };
+  }
+
+  if (storeAuth.user && to.name === 'auth') {
+    return false;
+  }
 });
 
 export default router;
